2d35062905f910b5a8f67141d7367af059975279
Joris Heyens <joris.heyens@doddle.be>
2022-08-31 11:42:57 +0200

    fix: validation on schema

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -144,10 +144,6 @@ router.post("/", auth.required, function(req, res, next) {
         return res.sendStatus(401);
       }
 
-      if (!item.image) {
-        return res.sendStatus(400);
-      }
-
       var item = new Item(req.body.item);
 
       item.seller = user;
